066780c9ce57d690f926ad21ad9cbcdacf696c85
JasonDClarke <jasondanielclarke@gmail.com>
2017-07-13 12:11:21 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+//const App = () => <h1>Hi</h1>;
+
+class App extends React.Component {
+    render() {
+        return (
+          <div>
+            <h1>Hello World</h1>
+            <strong>Strong</strong>
+          </div>
+          );
+    }
+}
+
+export default App;
